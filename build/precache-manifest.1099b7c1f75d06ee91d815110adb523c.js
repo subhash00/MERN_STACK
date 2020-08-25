c15e4644f952144896c0467a9eab61f751b2527a
subhash00 <subhahpathak1991@gmail.com>
2020-08-25 14:09:07 +0530

    MERN_STACK

---
+++ build/precache-manifest.1099b7c1f75d06ee91d815110adb523c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7dde3e654ba8fbe5ab580030a3b395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70905e08750027b46c1d",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "943865c39ad5d3ff1920",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "70905e08750027b46c1d",
+    "url": "/static/js/2.91524322.chunk.js"
+  },
+  {
+    "revision": "7406da88f10915801ef0485cc5cc90fd",
+    "url": "/static/js/2.91524322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943865c39ad5d3ff1920",
+    "url": "/static/js/main.8c2bb77e.chunk.js"
+  },
+  {
+    "revision": "0646b6df8eae28ddd531",
+    "url": "/static/js/runtime-main.4dc90399.js"
+  }
+]);
