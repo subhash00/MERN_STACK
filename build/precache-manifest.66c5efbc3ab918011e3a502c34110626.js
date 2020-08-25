30f0b0ccebcd4a713f6e46aa41e5922b20917d06
subhash00 <subhahpathak1991@gmail.com>
2020-08-25 15:29:27 +0530

    Mern stack

---
+++ build/precache-manifest.66c5efbc3ab918011e3a502c34110626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7399c25f4f96d4d52137e355cf361738",
+    "url": "/MERN_STACK/index.html"
+  },
+  {
+    "revision": "1b4490ed1c1d44765a65",
+    "url": "/MERN_STACK/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "6d5918f1689b2c3c678e",
+    "url": "/MERN_STACK/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "1b4490ed1c1d44765a65",
+    "url": "/MERN_STACK/static/js/2.19cc8afc.chunk.js"
+  },
+  {
+    "revision": "7406da88f10915801ef0485cc5cc90fd",
+    "url": "/MERN_STACK/static/js/2.19cc8afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5918f1689b2c3c678e",
+    "url": "/MERN_STACK/static/js/main.f77261b0.chunk.js"
+  },
+  {
+    "revision": "add3931be634a8bb1f2a",
+    "url": "/MERN_STACK/static/js/runtime-main.93e3b5f1.js"
+  }
+]);
