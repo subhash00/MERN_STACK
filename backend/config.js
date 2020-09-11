33f6178bdc3bf52a15287eee0eb281cca834f5a1
subhash pathak <66675521+subhash00@users.noreply.github.com>
2020-09-11 21:11:36 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://subhash:subhashpathak@cluster0-koesa.gcp.mongodb.net/dashboard?retryWrites=true&w=majority',
-  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  MONGODB_URL: process.env.MONGODB_URL || 'your mongodb secret key',
+  JWT_SECRET: process.env.JWT_SECRET || 'yoursecret',
 };
 
